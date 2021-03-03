584b9c5f676fee4432627c7d3375f50e4d4618ff
maheshwarishikha <maheshwari.shikha@gmail.com>
2021-03-03 17:18:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ if (process.env.PORT) {
 
 var app = express();
 app.get('/', function (req, res) {
-  console.log("Test Repo Empty toolchain");
+  console.log("Test Repo Empty toolchain-1");
   res.send(message.getWelcomeMessage());
 });
 
